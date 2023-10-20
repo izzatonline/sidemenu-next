593056d4ffd18757fef70e2baabe42bccc931742
Izzat J <izzat.online@gmail.com>
2023-10-21 06:48:55 +0800

    Test long page

--- src/app/projects/web-design/page.tsx
+++ src/app/projects/web-design/page.tsx
@@ -7,6 +7,30 @@ const WebDesignPage = () => {
 
             <div className="border-dashed border border-zinc-500 w-full h-12 rounded-lg"></div>
             <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
+
+            <div className="border-dashed border border-zinc-500 w-full h-12 rounded-lg"></div>
+            <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
+
+            <div className="border-dashed border border-zinc-500 w-full h-12 rounded-lg"></div>
+            <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
+
+            <div className="border-dashed border border-zinc-500 w-full h-12 rounded-lg"></div>
+            <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
+
+            <div className="border-dashed border border-zinc-500 w-full h-12 rounded-lg"></div>
+            <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
+
+            <div className="border-dashed border border-zinc-500 w-full h-12 rounded-lg"></div>
+            <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
+
+            <div className="border-dashed border border-zinc-500 w-full h-12 rounded-lg"></div>
+            <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
+
+            <div className="border-dashed border border-zinc-500 w-full h-12 rounded-lg"></div>
+            <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
+
+            <div className="border-dashed border border-zinc-500 w-full h-12 rounded-lg"></div>
+            <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
         </>
     );
 };
