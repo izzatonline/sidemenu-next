e600878b652e3cab8df37223e8067e8159ff6099
Izzat J <izzat.online@gmail.com>
2023-10-21 06:43:24 +0800

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div>
+            <h1 className=" text-3xl font-bold">
+                Hello! My Name is Izzat! Nice to meet you!
+            </h1>
+        </div>
+    );
+}
